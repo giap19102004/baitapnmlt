698c95fe186fa84707d627bf43c4283188d2c21c
giap19102004 <114390983+giap19102004@users.noreply.github.com>
2022-12-07 01:09:32 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+function showMessage(){
+    alert('Xin chào!')}
+var a= null;
+var Pi= 3.14;
+var c=null
+let R= prompt("Nhập bán kính hình tròn");
+
+function M() {
+    let C= 2*Pi*R;
+    document.write("Chu vi: "+C);
+}
+function show() {
+    let S= Pi*R^2;
+    document.write("Diện tích: "+S);
+}
